f1b8801d9bd00692766f8d0ea499b9141c51fff3
Khushi Gupta <76203536+Khushigupta12@users.noreply.github.com>
2023-09-15 19:38:41 +0530

    Update Category.jsx

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -55,7 +55,7 @@ export const Category = () => {
 
   useEffect(() => {
     const getCat = async () => {
-      const res = await axios.get("/category" + search);
+      const res = await axios.get("https://api-2jif.onrender.com/category" + search);
       setCat(res.data);
     };
     getCat();
